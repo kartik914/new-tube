5af5eb5ea58798cb128bbac8b6d09e622b45b9dd
Kartik Bhatia <kartikbhatiaa12@gmail.com>
2025-02-16 21:25:32 +0530

    Created Default Pages

---
+++ src/app/(home)/feed/subscriptions/page.tsx
@@ -0,0 +1,12 @@
+const SubscriptionsPage = () => {
+  return (
+    <>
+      <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div className="hidden fixed top-0 right-0 px-6 py-4 sm:block"></div>
+        <div className="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">Subscriptions</div>
+      </div>
+    </>
+  );
+};
+
+export default SubscriptionsPage;
---
+++ src/app/(home)/playlists/history/page.tsx
@@ -0,0 +1,12 @@
+const HistoryPage = () => {
+  return (
+    <>
+      <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div className="hidden fixed top-0 right-0 px-6 py-4 sm:block"></div>
+        <div className="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">History</div>
+      </div>
+    </>
+  );
+};
+
+export default HistoryPage;
