6225295055c56c162ad42bb63091e96c61ed1b64
Kartik Bhatia <kartikbhatiaa12@gmail.com>
2025-04-08 21:06:41 +0530

    Fixed use-current-user hook

--- src/hooks/use-current-user.ts
+++ src/hooks/use-current-user.ts
@@ -3,10 +3,5 @@ import { useSession } from "next-auth/react";
 export const useCurrentUser = () => {
   const session = useSession();
 
-  if (!session.data?.user) {
-    console.error("No user found in session data", session.data);
-    throw new Error("User is not authenticated");
-  }
-
-  return session.data.user;
+  return session.data?.user || null;
 };
